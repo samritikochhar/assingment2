070fa6b9eb844f8f482f5f3cc437815c13700290
ramanbaghla89 <ramanbaghla89@gmail.com>
2018-08-20 10:43:44 +0530

    assingment2

---
+++ assingment 2.py
@@ -0,0 +1,46 @@
+#Q.1- Print anything you want on screen.
+
+print("hello Acadview")
+
+"""Q.2- Join two strings using '+'. 
+E.g.-"Acad"+"View” """
+
+a='Acad'
+b='View'
+print(a+b)
+
+
+#Q.3- Take the input of 3 variables x, y and z . Print their values on screen. \
+
+a=input(" ")
+b=input(" ")
+c=input(" ")
+print(a)
+print(b)
+print(c)
+
+
+#Q.4- Print “Let’s get started” on screen.
+print(""""Let\'s get started""""")
+
+
+"""Q.5- Print the following values using placeholders. 
+s=”Acadview” 
+course=”Python” 
+fees=5000 """
+
+s='Acadview' 
+course='Python' 
+fees=5000
+print("%s %s %d" %(s,course,fees))
+
+'''Q.6- Find the area of circle 
+pi = 3.14 
+Take radius as input from user 
+Print the area of circle'''
+
+r=int(input("entre the radius:"))
+pi=3.14
+Area=r*r*pi
+print(Area)
+
